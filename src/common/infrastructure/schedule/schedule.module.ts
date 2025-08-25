69e286e432e1835e38f19ba40b80fbdd2b63fea2
Pedro Tatibano <pedrotatibano190@gmail.com>
2025-08-25 15:18:25 -0300

    refactor: remove ScheduleAppModule and PartitionService due to redundancy

--- src/common/infrastructure/schedule/schedule.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { ScheduleModule } from "@nestjs/schedule";
-import { PartitionService } from "./services/partition.service";
-
-@Module({
-  imports: [ScheduleModule.forRoot()],
-  providers: [PartitionService],
-})
-export class ScheduleAppModule {}
