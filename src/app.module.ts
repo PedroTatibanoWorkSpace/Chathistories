423c5d3ab062f491e68a58d25f91755a3e4366ed
Pedro Tatibano <pedrotatibano190@gmail.com>
2025-08-24 23:01:57 -0300

    feat: add scheduling module and partition service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './common/infrastructure/database/database.module';
+import { ScheduleAppModule } from './common/infrastructure/schedule/schedule.module';
+import { AccountModule } from './modules/accounts/account.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    DatabaseModule,
+    ScheduleAppModule,
+    AccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
